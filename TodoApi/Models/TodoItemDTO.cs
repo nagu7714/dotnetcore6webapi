5bc663e4103e2f174e7fd5eab6e183eb4d1720a6
nagaraj.mca2009@gmail.com <nagaraj.mca2009@gmail.com>
2022-06-05 16:05:17 +0530

    created DTO  to Prevent over-posting

--- TodoApi/Models/TodoItemDTO.cs
+++ TodoApi/Models/TodoItemDTO.cs
@@ -1,14 +1,13 @@
 ﻿namespace TodoApi.Models
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
+
         public long Id { get; set; }
 
         public string? Name { get; set; }
 
         public bool IsComplete { get; set; }
-
-        public string? Secret { get; set; }
 
     }
 }
