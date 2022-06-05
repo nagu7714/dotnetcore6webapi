1054cc0840d5b90a101376e8bbd89cd85bc03e71
nagaraj.mca2009@gmail.com <nagaraj.mca2009@gmail.com>
2022-06-05 13:20:53 +0530

    Removed Swagger & Configured In Memory DB Context

---
+++ TodoApi/DBContexts/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using TodoApi.Models;
+namespace TodoApi.DBContexts
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> ToDoItems { get; set; } = null!;
+    }
+}
